7153e1656949ba080f4950fea85c97cb522edf2a
Gustavo <guhenriques1398@gmail.com>
2023-07-31 12:41:48 +0100

    Project v1.0

--- src/components/NoteComponents/Notes.js
+++ src/components/NoteComponents/Notes.js
@@ -1,20 +1,67 @@
 import { React, useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
 import "../css/Note.css";
 import Note from "./Note";
-
 import CreateNote from "./CreateNote";
 
 
 function Notes() {
-  //states
+  // states
   const [notes, setNotes] = useState([]);
   const [inputText, setInputText] = useState("");
 
+  // get text and store in state
+  const textHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  // add new note to the state array
+  const saveHandler = () => {
+    setNotes((prevState) => [
+      ...prevState,
+      {
+        id: uuid(),
+        text: inputText,
+      },
+    ]);
+    // clear the textarea
+    setInputText("");
+  };
+
+  // delete note function
+  const deleteNote = (id) => {
+    const filteredNotes = notes.filter((note) => note.id !== id);
+    setNotes(filteredNotes);
+  };
+
+  //get the saved notes and add them to the array
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("Notes"));
+    if (data && data.length > 0 ) {
+      setNotes(data);
+    }
+  }, []);
+
+  //saving data to local storage
+  useEffect(() => {
+    localStorage.setItem("Notes", JSON.stringify(notes));
+  }, [notes]);
+
   return (
     <div className="notes">
-      <Note />
-      <Note />
-      <CreateNote />
+      {notes.map((note) => (
+        <Note
+          key={note.id}
+          id={note.id}
+          text={note.text}
+          deleteNote={deleteNote}
+        />
+      ))}
+      <CreateNote
+        textHandler={textHandler}
+        saveHandler={saveHandler}
+        inputText={inputText}
+      />
     </div>
   );
 }
