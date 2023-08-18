9921ca0264c65ac3ab1b9fdd7821c9cb9faaea04
Gustavo <guhenriques1398@gmail.com>
2023-08-18 15:05:21 +0100

    Notes/setNotes prop being passed in App main

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import CreateNote from "./components/NoteComponents/CreateNote";
+import EditNote from "./components/NoteComponents/EditNote";
 import Header from "./components/NoteComponents/Header";
 import Notes from "./components/NoteComponents/Notes";
 
 import "./components/css/App.css";
 
 function App() {
-
   const [darkMode, setDarkMode] = useState(false);
-
   const handleDarkModeToggle = () => {
     setDarkMode((prevMode) => !prevMode);
   };
+
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("Notes"));
+    if (data && data.length > 0) {
+      setNotes(data);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("Notes", JSON.stringify(notes));
+  }, [notes]);
+
 
   return (
     <div className={darkMode ? "dark-mode" : "white-mode"}>
@@ -21,9 +34,9 @@ function App() {
       <main id="app">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Notes />} />
+            <Route path="/" element={<Notes notes={notes} setNotes={setNotes} />} />
             {/*<Route path="/create-note" element={<CreateNote />} /> */}
-            {/*<Route path="/edit-note/:id" element={<Notes />} />*/}
+            <Route path="/edit-note/:id" element={<EditNote />} />
           </Routes>
         </BrowserRouter>
       </main>
