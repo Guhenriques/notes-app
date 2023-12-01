c9f1738f8f67cc43e562fe5492bc05ccfc70948e
Guhenriques <guhenriques1398@gmail.com>
2023-12-01 17:48:19 +0000

    Major changes and fix edit note function

--- src/components/NoteComponents/Notes.jsx
+++ src/components/NoteComponents/Notes.jsx
@@ -1,24 +1,29 @@
-import { React, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuid } from "uuid";
 import "../css/Note.css";
 import Note from "./Note";
 import CreateNote from "./CreateNote";
 import Search from "./Search";
 
-
-const Notes = ({ notes, setNotes }) => {
-  // states
+const Notes = () => {
+  const [notes, setNotes] = useState(() => {
+    const localStorageNotes = JSON.parse(localStorage.getItem("Notes"));
+    return localStorageNotes || [];
+  });
+  
   const [inputText, setInputText] = useState("");
-  const [editText, setEditText] = useState("");
   const [searchText, setSearchText] = useState("");
-  const [editingNoteId, setEditingNoteId] = useState(null); // Track the note being edited
+  const [editingNoteId, setEditingNoteId] = useState('');
 
-  // get text and store in state
+  // Second useEffect
+  useEffect(() => {
+    localStorage.setItem("Notes", JSON.stringify(notes));
+  }, [notes]);
+
   const textHandler = (e) => {
     setInputText(e.target.value);
   };
 
-  // Create note function
   const createNote = () => {
     if (inputText.trim().length > 0) {
       setNotes((prevNotes) => [
@@ -28,54 +33,44 @@ const Notes = ({ notes, setNotes }) => {
           text: inputText,
         },
       ]);
-      setInputText(""); // Clear the textarea
+      setInputText("");
     }
   };
 
-  // Delete note function
   const deleteNote = (id) => {
     const filteredNotes = notes.filter((note) => note.id !== id);
     setNotes(filteredNotes);
   };
 
-
-  // Enter edit mode
-  const startEdit = (id, initialText) => {
-    setEditingNoteId(id); // Set the ID of the note being edited
-    setEditText(initialText); // Initialize the edit text with the note's text
+  const startEdit = (note) => {
+    setEditingNoteId(note.id);
+    setInputText(note.text); // Set the input text to the note's text
   };
 
-  const handleSaveChanges = () => {
-    if (editText.trim().length > 0 && editingNoteId) {
+  const handleSaveChanges = (id, editedText) => {
+    if (editedText.trim().length > 0 && id) {
       const updatedNotes = notes.map((note) => {
-        if (note.id === editingNoteId) {
-          return { ...note, text: editText };
+        if (note.id === id) {
+          return { ...note, text: editedText };
         }
         return note;
       });
 
-      console.log("Editing Note ID:", editingNoteId);
-      console.log("Updated Notes:", updatedNotes);
+      console.log('updatedNotes:', updatedNotes)
 
       setNotes(updatedNotes);
       setEditingNoteId(null);
-      setEditText([]);
+      setInputText('');
+
+      // Update local storage
+      localStorage.setItem("Notes", JSON.stringify(updatedNotes));
     }
   };
 
-  useEffect(() => {
-    const localStorageNotes = JSON.parse(localStorage.getItem("notes"));
-    if (localStorageNotes) {
-      setNotes(localStorageNotes);
-    }
-  }, []);
-
-  // Filter notes based on search text
   const filteredNotes = notes
     ? notes.filter((note) =>
-      note.text.toLowerCase().includes(searchText.toLowerCase())
-    )
-
+        note.text.toLowerCase().includes(searchText.toLowerCase())
+      )
     : [];
 
   return (
@@ -88,9 +83,9 @@ const Notes = ({ notes, setNotes }) => {
             id={note.id}
             text={note.text}
             deleteNote={deleteNote}
-            startEdit={startEdit} // Pass the startEdit function to begin editing
+            startEdit={() => startEdit(note)}
             isEditing={editingNoteId === note.id}
-            handleSaveChanges={handleSaveChanges} // Pass the function to save changes
+            handleSaveChanges={handleSaveChanges}
           />
         ))}
         <CreateNote
@@ -98,11 +93,9 @@ const Notes = ({ notes, setNotes }) => {
           saveHandler={createNote}
           inputText={inputText}
         />
-
-
       </div>
     </div>
   );
-}
+};
 
 export default Notes;
