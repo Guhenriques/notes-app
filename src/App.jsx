7eed1bea4050d036d09fc01fd3be8da9da0e6c50
Guhenriques <guhenriques1398@gmail.com>
2024-02-29 23:12:42 +0000

    update dark/light modes to persist

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,30 @@ import Notes from "./components/NoteComponents/Notes";
 import "./components/css/App.css";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  // store current mode in localStorage
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedMode = localStorage.getItem("darkMode");
+    return savedMode ? JSON.parse(savedMode) : false;
+  });
+
+  // update mode initial state based on localStorage
+  useEffect(() =>{
+    const savedMode = localStorage.getItem("darkMode");
+    if (savedMode) {
+      setDarkMode(JSON.parse(savedMode));
+    }
+  }, []);
+
+  // update mode state whenever user switch between modes
   const handleDarkModeToggle = () => {
-    setDarkMode((prevMode) => !prevMode);
+    setDarkMode((prevMode) => {
+      const newMode = !prevMode;
+      localStorage.setItem("darkMode", JSON.stringify(newMode))
+      return newMode;
+    })
   };
 
   const [notes, setNotes] = useState([]);
-
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem("Notes"));
     if (data && data.length > 0) {
@@ -30,6 +47,11 @@ function App() {
     localStorage.setItem("Notes", JSON.stringify(notes));
   }, [notes]);
 
+  useEffect(() => {
+    console.log(`Is in dark mode? ${darkMode}`);
+  }, [darkMode]);
+
+  console.log("notes:", notes)
 
   return (
     <div className={darkMode ? "dark-mode" : "white-mode"}>
